a9195da969cf54a1fc0b14bc997f37deb97ed517
Legit-source <tomnev@post.bgu.ac.il>
2025-01-21 17:53:48 +0000

    initial commit 2 because i fucked up the first one

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtocol.java
@@ -2,14 +2,17 @@ package bgu.spl.net.api;
 
 import bgu.spl.net.srv.Connections;
 
-public interface StompMessagingProtocol<T>  {
+public interface StompMessagingProtocol<T> extends MessagingProtocol<T>{
 	/**
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
 	**/
     void start(int connectionId, Connections<T> connections);
-    
-    void process(T message);
-	
+    /**
+     * process the given message 
+     * @param msg the received message
+     * @return the response to send or null if no response is expected by the client
+     */
+    T process(T msg);
 	/**
      * @return true if the connection should be terminated
      */
--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,32 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.impl.echo.EchoProtocol;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
+import bgu.spl.net.srv.Server;
+
 public class StompServer {
 
     public static void main(String[] args) {
-        // TODO: implement this
+        if (args.length < 2) {
+            return;
+        }
+
+        if (args[1] == "tpc") {
+            Server.threadPerClient(
+                    Integer.parseInt(args[0]), // port
+                    () -> new EchoProtocol(), // protocol factory
+                    LineMessageEncoderDecoder::new // message encoder decoder factory
+            ).serve();
+        }
+
+        else if (args[1] == "reactor") {
+            Server.reactor(
+                    Runtime.getRuntime().availableProcessors(),
+                    7777, // port
+                    () -> new EchoProtocol<>(), // protocol factory
+                    LineMessageEncoderDecoder::new // message encoder decoder factory
+            ).serve();
+        }
+        return;
     }
 }
